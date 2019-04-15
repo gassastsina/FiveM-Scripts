a5a8ac4df061ff753260a2885f6fca5b668b6380
gassastsina <36570887+gassastsina@users.noreply.github.com>
2019-04-15 21:20:07 +0200

    Add files via upload

---
+++ resources/[esx]/esx_property/config.lua
@@ -0,0 +1,11 @@
+Config                        = {}
+Config.DrawDistance           = 50
+Config.MarkerSize             = {x = 1.5, y = 1.5, z = 0.7}
+Config.MarkerColor            = {r = 102, g = 102, b = 204}
+Config.RoomMenuMarkerColor    = {r = 102, g = 204, b = 102}
+Config.MarkerType             = 1
+Config.Zones                  = {}
+Config.Properties             = {}
+Config.EnablePlayerManagement = false -- If set to true you need esx_realestateagentjob
+Config.Locale                 = 'fr'
+Config.AgentImmo			  = {x = 291.979, y = -162.481, z = 63.619}
